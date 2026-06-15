efa0d50edcf841ffa0049862b21ac742c8c0a831
Your Name <chaithanyarajpeddireddy@gmail>
2026-06-15 18:12:40 +0530

    Fix Render allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/product-catalog/",  
+  base: "/product-catalog/",
+  preview: {
+    allowedHosts: true
+  }
 })
